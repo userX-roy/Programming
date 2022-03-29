cfb53db3bd82a184d089a12990c3e31549cb9031
Saikat <saikat.github@gmail.com>
2022-03-29 08:46:19 +0530

    Factorial program without recursion added

---
+++ C/factorial_wOut-recursion.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int fact(int no)
+{
+	int sum = 1;
+	for(int i = no;i>=1;i--)
+	{
+		sum = sum*i;
+	}
+	return sum;
+}
+/*fact() func execution for input 3 :
+  no = 3
+  for(3[as i is initialized with 3])
+  sum(3) = sum(1)*3
+  
+  for(2[as i is dectrement by 1])
+  sum(6) = sum(3)*2
+............ and so on
+
+so the execution will be like "fact(n) = n*(n-1)*(n-2)*.....*1"
+*/
+int main()
+{
+	int no;
+	printf("Enter a no. to calculate the factorial : ");
+	scanf("%d",&no);
+	printf("The factorial of %d is : %d\n",no,fact(no));
+	return 0;
+}
