cbb0ff382a157ad1fdff9e2cba41206d934217db
Saikat <saikat.github@gmail.com>
2022-03-29 09:23:37 +0530

    Factorial-calculation using recursion added

---
+++ C/factorial_with-recursion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int facto (int);
+int main()
+{
+	int no;
+	printf("Enter a number to calculate the factorial : ");
+	scanf("%d",&no);
+	printf("Factorial of %d is %d\n",no,facto(no));
+	return 0;
+}
+
+int facto (int x)
+{
+	int fact = 0;
+	if(x==0)
+		return 1;
+	else
+	{
+	fact = x * facto(x-1);
+	return fact;
+	}
+}
