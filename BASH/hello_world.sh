60faeb6934724e030cdae0f0f325a776241d661c
Saikat <saikat.github@gmail.com>
2022-04-01 22:24:51 +0530

    From today Bash Scripting files are started to add

---
+++ BASH/hello_world.sh
@@ -0,0 +1,10 @@
+#/usr/bin/bash
+
+
+#This is a single line comment 
+
+: 'This is a 
+Multiline Commen '
+# the space between : and ' is must otherwise there will be an error
+
+echo "This ia start of bash scripting"
